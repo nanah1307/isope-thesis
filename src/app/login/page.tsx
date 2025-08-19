fb8aae9890a4e97afe0dbbfb7b5c57b42d43e48f
hanslope12 <91108047+hanslope12@users.noreply.github.com>
2025-08-19 15:49:22 +0800

    update ui design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,14 +5,18 @@ insert images
 */
 import LoginForm from '@/app/ui/login-form';
 import { Suspense } from 'react';
- 
+import NavBar from '@/app/ui/navbar';
+
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-          </div>
+     <main className="flex flex-col md:h-screen bg-gray-50">
+      <div className='flex h-20 w-full items-center justify-center border-2 border-solid border-blue-500'>
+        <NavBar/>
+      </div>
+      <div className="mx-auto flex w-full max-w-[400px] flex-col border-10 border-blue-500 rounded-2xl space-y-2.5 p-4 mt-5 ">
+        <div className="flex items-end h-10 w-full border-b-5 border-blue-500 p-3 md:h-20">
+          <p className='max-w-full mx-auto text-center text-black font-bold'>iAcademy <br />
+          iSope Online</p>
         </div>
         <Suspense>
           <LoginForm />
--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -1,6 +1,6 @@
 export default function LoginForm() {
   return (
-    <form className="flex flex-col space-y-4">
+    <form className="flex mt-5 flex-col space-y-4">
       <label className="block">
         <span className="text-sm font-medium text-gray-700">Email</span>
         <input
@@ -17,11 +17,18 @@ export default function LoginForm() {
           className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
         />
       </label>
+      
       <button
         type="submit"
-        className="w-full rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        className="w-full mt-10 rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
       >
         Log in
+      </button>
+      <button
+        type="submit"
+        className="w-full mt-3 rounded-md bg-red-600 px-4 py-2 text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+      >
+        Log in with Google
       </button>
     </form>
   );
