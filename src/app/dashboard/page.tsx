f74a0885a0e870138437188da755da04f4812d48
hanslope12 <91108047+hanslope12@users.noreply.github.com>
2025-10-04 13:02:15 +0800

    Generate page templates

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Osasboard from '@/app/ui/osas/osasboard';
+
+export default function dashboard() {
+  return <div>
+    <h1>Dashboard</h1>
+    <p>Welcome to the dashboard!</p>
+    <Osasboard/>
+  </div>
+}
---
+++ src/app/ui/osas/osasboard.tsx
@@ -0,0 +1,3 @@
+export default async function Osasboard() {
+  return <div>Osasboard</div>;
+}
