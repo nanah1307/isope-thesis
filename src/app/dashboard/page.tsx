c73f43008128f936f02bfe8cc42e57c9fe1d2d1c
hanslope12 <91108047+hanslope12@users.noreply.github.com>
2025-10-05 21:30:20 +0800

    Explained whatever

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,8 @@
 import Osasboard from '@/app/ui/osas/osasboard';
-
+import Osasbar from '../ui/osas/osasbar';
 export default function dashboard() {
   return <div>
+    <Osasbar/>
     <h1>Dashboard</h1>
     <p>Welcome to the dashboard!</p>
     <Osasboard/>
