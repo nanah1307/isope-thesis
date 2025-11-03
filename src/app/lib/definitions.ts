4f315d6e6c77c2deb92cd2980e6388ef4222d10c
cheesy-lasagna <nicolepecho9174@gmail.com>
2025-11-03 20:23:37 +0800

    Revert "Merge pull request #3 from nanah1307/development"
    This reverts commit 0616e37151a59343ff6f8a44149f34f5b48776c8, reversing
    changes made to 53adadd5c78438fb22f1229cee88f17d40686a97.

--- src/app/lib/definitions.ts
+++ src/app/lib/definitions.ts
@@ -1,7 +1,20 @@
-export type OrgsProps = {
-  orgs: {
+export type orgProp = {
+  id: number;
+  name: string;
+  price: number;
+  stock: number;
+  details: string;
+  categories: string;
+  photo: string;
+};
+
+export type Cart = {
+  CartItems: Array<{
+    id: number;
     name: string;
-    bio: string;
-    avatar: string;
-  };
+    price: number;
+    details: string;
+    photo: string;
+    quantity: number;
+  }>;
 };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,23 @@
-"use client";
+/*import AcmeLogo from '@/app/ui/acme-logo';
+import LoginForm from '@/app/ui/login-form';
 
-import { signIn, signOut, useSession } from "next-auth/react";
-
-export default function Home() {
-  const { data: session } = useSession();
-
-  if (session) {
-    return (
-      <div>
-        <p>Signed in as {session.user?.email}</p>
-        <button onClick={() => signOut()}>Sign out</button>
-      </div>
-    );
-  }
+insert images 
+*/
+import LoginForm from '@/app/ui/login-form';
+import { Suspense } from 'react';
+ 
+export default function LoginPage() {
   return (
-    <div>
-      <p>Not signed in</p>
-      <button onClick={() => signIn("google")}>Sign in with Google</button>
-    </div>
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+          </div>
+        </div>
+        <Suspense>
+          <LoginForm />
+        </Suspense>
+      </div>
+    </main>
   );
 }
