63012c2cc96f63db277cdf327443b4ff903682fa
4ths-coding <spareemail4th@gmail.com>
2025-11-16 23:36:54 +0800

    Update route.ts

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   // ⚠️ Replace 'products' with a table name that actually exists in your Supabase project
   // http://localhost:3000/api/test for sample
-  const { data, error } = await supabase.from("products").select("*").limit(1);
+  const { data, error } = await supabase.from("req").select("*");
 
   if (error) {
     return NextResponse.json(
