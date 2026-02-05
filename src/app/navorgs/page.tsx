2393ff54a955d17f0071e606bc7b043448d6d07c
nicolepecho <nicole.pecho@edufied.edu.vn>
2026-02-06 01:01:56 +0800

    created navorgs files page and layout

---
+++ src/app/navorgs/page.tsx
@@ -0,0 +1,7 @@
+export default function NavOrgsPage() {
+  return (
+    <div className="flex h-screen overflow-hidden bg-gradient-to-br from-blue-50 to-indigo-100">
+      this is the navorgs page. this will contain the list of orgs in detail similar to dashboard
+    </div>
+  );
+}
