a65771170c1b75d99b8f4f8550fe2a9e6669ee33
Giles2816 <Satsujinki5659@gmail.com>
2026-02-10 22:51:52 +0800

    Btn bottom right

--- src/app/ui/snippets/orgs/OrgsMembers.tsx
+++ src/app/ui/snippets/orgs/OrgsMembers.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-
 import { DocumentTextIcon  } from '@heroicons/react/24/outline';
 import {useEffect, useState, FC} from 'react';
 import React from "react";
@@ -9,49 +8,49 @@ import UploadMembersModal from "./uploadMembers";
 export default function OrgsMembers({ username }: {username:string}){
     const [members, setMembers] = useState<any[]>([]);
     const [showModal, setShowModal] = useState(false);
-
+    
     useEffect(() => {
-  const fetchmembers = async () => {
-    const { data, error } = await supabase.from('member').select('*').eq("organizations",username);
-    if (error) console.error('Error fetching member:', error.message);
-    else setMembers(data);  // Stores orgs in state
-  };
-  fetchmembers();
-}, [username]);
+      const fetchmembers = async () => {
+        const { data, error } = await supabase.from('member').select('*').eq("organizations",username);
+        if (error) console.error('Error fetching member:', error.message);
+        else setMembers(data);  // Stores orgs in state
+      };
+      fetchmembers();
+    }, [username]);
 
-return(
-<div className="overflow-x-auto" key="requirements-1">
-     <UploadMembersModal/>
-        {/*if view = */}
-        <table className="min-w-full border border-gray-300 bg-white text-black text-xs sm:text-sm md:text-base">
-          <thead>
-            <tr className="bg-white text-black">
-              <th className="border border-gray-300 px-3 py-2 text-left w-2/3">Member</th>
-              <th className="border border-gray-300 px-3 py-2 text-left">Evaluation</th>
-            </tr>
-          </thead>
-         
-          <tbody>
-                {members.map((member) => {
-                  return (
-                    <tr key={member.id} className="border-b border-gray-200">
-                      <td className="border px-3 py-2">{member.student_name}</td>
-                      <td className="border px-3 py-2">
-                        <Link
-                          href={`/dashboard/orgs/${username}/requirements/${member.id}`}
-                          className="text-blue-500 hover:underline"
-                        >
-                            <DocumentTextIcon className="w-10 h-8 inline mr-1"/> View
-                        </Link>
-                      </td>
-
-                    </tr>
-                  );
-                })}
-               
-          </tbody>
-        </table>
-     
+    return(
+      <div className="relative min-h-[400px]">
+        <div className="overflow-x-auto" key="requirements-1">
+          <table className="min-w-full border border-gray-300 bg-white text-black text-xs sm:text-sm md:text-base">
+            <thead>
+              <tr className="bg-white text-black">
+                <th className="border border-gray-300 px-3 py-2 text-left w-2/3">Member</th>
+                <th className="border border-gray-300 px-3 py-2 text-left">Evaluation</th>
+              </tr>
+            </thead>
+    
+            <tbody>
+              {members.map((member) => {
+                return (
+                  <tr key={member.id} className="border-b border-gray-200">
+                    <td className="border px-3 py-2">{member.student_name}</td>
+                    <td className="border px-3 py-2">
+                      <Link
+                        href={`/dashboard/orgs/${username}/requirements/${member.id}`}
+                        className="text-blue-500 hover:underline"
+                      >
+                        <DocumentTextIcon className="w-10 h-8 inline mr-1"/> View
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div className="absolute bottom-4 right-4">
+          <UploadMembersModal/>
+        </div>
       </div>
-)
+    )
 };
